ab2ecc99b461eb786d9eb19d2d9f42cd2a0fcda2
Вася <vasyamontenegro1985@gmail.com>
2023-12-01 19:26:12 +0300

    делаем питошку

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Типо обнова")
+from sys import stdin
+import json
+
+data = [line.strip() for line in stdin]
+D = {line.split(' == ')[0]: line.split(' == ')[1] for line in data[1:]}
+with open(data[0], 'r', encoding="UTF-8") as f:
+    records = json.load(f)
+records.update(D)
+if len(data) > 1:
+    with open(data[0], 'w', encoding="UTF-8") as f:
+        json.dump(records, f, ensure_ascii=False, indent=4, sort_keys=True)
